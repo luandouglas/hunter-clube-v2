74ed206fcbadcc5d9dccb4bfc9728c1d3d917ce4
Luan <douglasmaia.mn.sl@gmail.com>
2023-07-08 09:29:56 -0300

    fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -80,7 +80,7 @@ const Layout = (props: PropsWithChildren) => {
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Eventos</span>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  3
+                  4
                 </span>
               </a>
             </li>
